e0e3b9b24378c349e7460075a1a62fffd6b5f496
Dinud Fernando <85138545+Dinud1231@users.noreply.github.com>
2026-04-29 11:32:31 +0000

    Apr 29, 2026, 7:32 AM

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+# IMPORTS
 import os
 import json
 import pandas as pd
@@ -5,12 +6,13 @@ import numpy as np
 import streamlit as st
 
 
-
+# Path Helper
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def get_data_path(filename: str) -> str:
     return os.path.join(APP_PATH, "data", filename)
 
+# Page Details
 st.set_page_config(
     page_title="Budget Index",
     page_icon="🪙",
