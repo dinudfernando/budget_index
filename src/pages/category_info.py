0216c51c5e5b2f962249fd99960e722da0edd6ea
Dinud Fernando <85138545+Dinud1231@users.noreply.github.com>
2026-05-04 15:20:47 +0000

    May 4, 2026, 11:20 AM

--- src/pages/category_info.py
+++ src/pages/category_info.py
@@ -14,5 +14,19 @@ def get_data_path(filename: str) -> str:
 
 st.set_page_config(
     page_title="Category Info",
-    page_icon=""
+    page_icon="📉",
+    layout="wide"
 )
+
+TIMEFRAMES = ["W","M","Y","2Y","5Y"]
+
+@st.cache_data
+def load_transactions() -> pd.DataFrame:
+    path = get_data_path("transactions.json")
+    with open(path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    df = pd.DataFrame(data)
+    df["date"] = pd.to_datetime(df["date"])
+    df["amount"] = pd.to_numeric(df["amount"])
+    return df.sort_values("date")
