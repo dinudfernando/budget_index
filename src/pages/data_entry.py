104f06415c824e187716e2da26e2729fb528f778
Dinud Fernando <85138545+Dinud1231@users.noreply.github.com>
2026-05-06 14:15:30 +0000

    May 6, 2026, 10:15 AM

--- src/pages/data_entry.py
+++ src/pages/data_entry.py
@@ -5,10 +5,14 @@ from datetime import date
 import pandas as pd
 import streamlit as st
 
-
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
 
 #Path finder
 def get_data_path(filename: str) -> str:
     return os.path.join(APP_PATH, "../data", filename)
 
+st.set_page_config(
+    page_title="New Data Entry",
+    page_icon="➕",
+    layout="wide"
+)
