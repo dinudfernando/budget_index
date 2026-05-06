063222b4923d4d00d195e5334fe3119f11a71d4a
Dinud Fernando <85138545+Dinud1231@users.noreply.github.com>
2026-05-06 16:45:27 +0000

    May 6, 2026, 12:45 PM

--- src/pages/category_info.py
+++ src/pages/category_info.py
@@ -232,11 +232,11 @@ with comparison_col:
     #More info stats under comparisons
     c1,c2 = st.columns(2)
     c1.metric("Standing", f'#{standing}')
-    c2.metric("Portion", f"{portion:,.1f}")
+    c2.metric("Portion", f"{portion:,.1f}%")
 
     c3,c4 = st.columns(2)
-    c3.metric("Category Total", f'#{selected_total}')
-    c4.metric("Group Total", f"{group_total:,.1f}")
+    c3.metric("Category Total", f'${selected_total}')
+    c4.metric("Group Total", f"${group_total:,.1f}")
 
 st.subheader("View History")
 
